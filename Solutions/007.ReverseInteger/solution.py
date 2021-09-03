ea3bc7dcf22790dc2089d33b3065b22ec36f41c3
David Ewald <ewalddav@gmail.com>
2021-09-03 10:38:28 -0700

    fast version for 007, but it seems still not as fast as it can be, maybe some sort of optimization exists here still

--- Solutions/007.ReverseInteger/solution.py
+++ Solutions/007.ReverseInteger/solution.py
@@ -8,13 +8,14 @@
     Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
 '''
+import math
 
 class Solution:
     '''
         This could probably be faster if we popped off the integers without string conversion
         Note: We can check bit integer range using '-1 << 31 and 1 << 31'
     '''
-    def reverse(self, x: int) -> int:
+    def reverseSlow(self, x: int) -> int:
         num = str(abs(x))
         if x < 0:
             num = num[::-1]
@@ -27,3 +28,25 @@ class Solution:
             return 0
         
         return x
+
+    '''
+        This does not use string conversion, seems faster, instead we chop digits using modulo and floor division.
+        We make sure to check if we are going to overflow first.
+    '''
+    def reverseFast(self, x: int) -> int:
+        min = -2147483648
+        max = 2147483647
+        
+        num = 0
+        while x:
+            digit = int(math.fmod(x, 10))
+            x = int(x / 10)
+
+            if (num > max // 10 or (num == max // 10 and digit >= max % 10)):
+                return 0
+            
+            if (num < min // 10 or (num == min // 10 and digit <= min % 10)):
+                return 0
+            num = (num * 10) + digit
+        
+        return num
