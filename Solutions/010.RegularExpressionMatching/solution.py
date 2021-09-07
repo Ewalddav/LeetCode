5077fe221b5cd67b5b2f35ec98b75054adccf7bc
David Ewald <ewalddav@gmail.com>
2021-09-06 21:32:23 -0700

    I wasted too much time on this, its truly sad, and there is still an edge case I cant handle even though this is what all the examples have for an answer

--- Solutions/010.RegularExpressionMatching/solution.py
+++ Solutions/010.RegularExpressionMatching/solution.py
@@ -12,7 +12,30 @@
 
 class Solution:
     '''
-        Here we go!
+        Magic, dynamic programming, still need to dig into this one more
     '''
     def isMatch(self, s: str, p: str) -> bool:
-        
+        boolMatrix = [[False for i in range(len(p) + 1)] for j in range(len(s) + 1)]
+
+        boolMatrix[0][0] = True
+
+        for j in range(1, len(boolMatrix[0])):
+            if p[j - 1] == '*':
+                boolMatrix[0][j] = boolMatrix[0][j - 2]
+
+        for i in range(1, len(boolMatrix)):
+            for j in range(1, len(boolMatrix[i])):
+                if p[j - 1] == '*':
+                    boolMatrix[i][j] == boolMatrix[i][j - 2]
+
+                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
+                        boolMatrix[i][j] == boolMatrix[i][j] or boolMatrix[i - 1][j]
+  
+                elif p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    boolMatrix[i][j] = boolMatrix[i - 1][j - 1]
+
+        return boolMatrix[-1][-1]
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.isMatch('aa', 'a*')
